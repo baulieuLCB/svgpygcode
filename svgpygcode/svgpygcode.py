8a9382bb7b362b59c0401f5383f7d6d8bb07371c
BaulieuLCB <baulieu@lcb-industries.com>
2018-11-16 21:51:44 +0100

    first profile / procket algorithm : only able to cut incrementally following the svg path

--- svgpygcode/svgpygcode.py
+++ svgpygcode/svgpygcode.py
@@ -1,5 +1,7 @@
 # svgpygcode
 
+import math
+
 class Machining:
     def __init__(self):
         # list of contours
@@ -17,7 +19,7 @@ class Machining:
             arguments:
                 - svg_path:str 'd' attribute of your path component
                 - operation_type:str description of the operation : 'profile_inside', 'profile_outside', 'pocket_inside', 'pocket_outside', 'engraving'
-                - properties:dict contains the machining characteristics : target_depth, cut_feedrate, plunge_feedrate, drill_type, drill_radius, depth_increment, stock_surface, holding_tabs_height, holding_tabs_number, holding_tabs_width
+                - properties:dict contains the machining characteristics : target_depth, cut_feedrate, plunge_feedrate, drill_type, drill_radius, depth_increment, stock_surface, clearance_pane, holding_tabs_height, holding_tabs_number, holding_tabs_width
         '''
         self.contours.append([operation_type, svg_path, properties])
 
@@ -31,7 +33,6 @@ class Machining:
         self.gcode = "G90\n"
         self.determine_order(priority)
         for i in self.order:
-            print(self.contours[i])
             if self.contours[i][0] == 'pocket_inside':
                 self.pocket(self.contours[i][1], self.contours[i][0], self.contours[i][2])
             if self.contours[i][0] == 'pocket_outside':
@@ -58,10 +59,51 @@ class Machining:
             arguments:
                 - svg_path:str 'd' attribute of your path component
                 - operation_type:str description of the operation : 'profile_inside', 'profile_outside', 'pocket_inside', 'pocket_outside', 'engraving'
-                - properties:dict contains the machining characteristics : target_depth, cut_feedrate, plunge_feedrate, drill_type, drill_radius, depth_increment, stock_surface, holding_tabs_height, holding_tabs_number, holding_tabs_width
+                - properties:dict contains the machining characteristics : target_depth, cut_feedrate, plunge_feedrate, drill_type, drill_radius, depth_increment, stock_surface, clearance_pane, holding_tabs_height, holding_tabs_number, holding_tabs_width
         '''
-        self.gcode += ("ajout d'un profil de type :\n" + type + "\n")
-        self.current_position = [1, 1]
+        profile = self.parse_path(svg_path)
+        properties = self.define_properties(properties)
+
+        # searching for the closest point from current position
+        closest_index = self.closest_index(profile, self.current_position)
+
+        # bringing the machining head to the closest point
+        temp = """G0 X{} Y{} Z{}\n""".format(self.current_position[0], self.current_position[1], properties['clearance_pane'])
+        if profile[closest_index][0] in ['M', 'L']:
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][0], profile[closest_index][1][1], properties['clearance_pane'])
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][0], profile[closest_index][1][1], properties['depth_increment'] if properties['depth_increment'] > properties['target_depth'] else properties['target_depth'])
+        elif profile[closest_index][0] in ['A']:
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][5], profile[closest_index][1][6], properties['clearance_pane'])
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][5], profile[closest_index][1][6], properties['depth_increment'] if properties['depth_increment'] > properties['target_depth'] else properties['target_depth'])
+        else:
+            raise ValueError('UNEXPECTED CURVE TYPE IN THE SVG - COULD NOT GENERATE GCODE. Sorry bro :-( . Happened while generating a profile')
+        self.gcode += temp
+
+        increment = 1
+        for increment in range(1, int(properties['target_depth']/properties['depth_increment']) + 2):
+            depth = increment * properties['depth_increment'] if increment * properties['depth_increment'] > properties['target_depth'] else properties['target_depth']
+            # plunging to the right depth
+            if profile[closest_index][0] in ['M', 'L']:
+                temp = """G1 X{} Y{} Z{}\n""".format(profile[closest_index][1][0], profile[closest_index][1][1], depth)
+            elif profile[closest_index][0] in ['A']:
+                temp = """G1 X{} Y{} Z{}\n""".format(profile[closest_index][1][5], profile[closest_index][1][6], depth)
+            self.gcode += temp
+            # go through the profile
+            for index in range(0, len(profile)):
+                # true index to work with : we are going to
+                i = (index + closest_index + 1)%len(profile)
+                if profile[i][0] in ['M', 'L']:
+                    temp = """G1 X{} Y{} Z{}\n""".format(profile[i][1][0], profile[i][1][1], depth)
+                if profile[i][0] == 'A':
+                    temp = """G1 X{} Y{} Z{}\n""".format(profile[i][1][5], profile[i][1][6], depth)
+                self.gcode += temp
+        if profile[closest_index][0] in ['M', 'L']:
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][0], profile[closest_index][1][1], properties['clearance_pane'])
+            self.current_position = [profile[closest_index][1][0], profile[closest_index][1][1]]
+        elif profile[closest_index][0] in ['A']:
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][5], profile[closest_index][1][6], properties['clearance_pane'])
+            self.current_position = [float(profile[closest_index][1][5]), float(profile[closest_index][1][6])]
+        self.gcode += temp
 
     def pocket(self, svg_path, type, properties):
         '''
@@ -69,10 +111,51 @@ class Machining:
             arguments:
                 - svg_path:str 'd' attribute of your path component
                 - operation_type:str description of the operation : 'profile_inside', 'profile_outside', 'pocket_inside', 'pocket_outside', 'engraving'
-                - properties:dict contains the machining characteristics : target_depth, cut_feedrate, plunge_feedrate, drill_type, drill_radius, depth_increment, stock_surface, holding_tabs_height, holding_tabs_number, holding_tabs_width
+                - properties:dict contains the machining characteristics : target_depth, cut_feedrate, plunge_feedrate, drill_type, drill_radius, depth_increment, stock_surface, clearance_pane, holding_tabs_height, holding_tabs_number, holding_tabs_width
         '''
-        self.gcode += ("ajout d'une poche de type :\n" + type + "\n")
-        self.current_position = [1, 1]
+        profile = self.parse_path(svg_path)
+        properties = self.define_properties(properties)
+
+        # searching for the closest point from current position
+        closest_index = self.closest_index(profile, self.current_position)
+
+        # bringing the machining head to the closest point
+        temp = """G0 X{} Y{} Z{}\n""".format(self.current_position[0], self.current_position[1], properties['clearance_pane'])
+        if profile[closest_index][0] in ['M', 'L']:
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][0], profile[closest_index][1][1], properties['clearance_pane'])
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][0], profile[closest_index][1][1], properties['depth_increment'] if properties['depth_increment'] > properties['target_depth'] else properties['target_depth'])
+        elif profile[closest_index][0] in ['A']:
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][5], profile[closest_index][1][6], properties['clearance_pane'])
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][5], profile[closest_index][1][6], properties['depth_increment'] if properties['depth_increment'] > properties['target_depth'] else properties['target_depth'])
+        else:
+            raise ValueError('UNEXPECTED CURVE TYPE IN THE SVG - COULD NOT GENERATE GCODE. Sorry bro :-( . Happened while generating a profile')
+        self.gcode += temp
+
+        increment = 1
+        for increment in range(1, int(properties['target_depth']/properties['depth_increment']) + 2):
+            depth = increment * properties['depth_increment'] if increment * properties['depth_increment'] > properties['target_depth'] else properties['target_depth']
+            # plunging to the right depth
+            if profile[closest_index][0] in ['M', 'L']:
+                temp = """G1 X{} Y{} Z{}\n""".format(profile[closest_index][1][0], profile[closest_index][1][1], depth)
+            elif profile[closest_index][0] in ['A']:
+                temp = """G1 X{} Y{} Z{}\n""".format(profile[closest_index][1][5], profile[closest_index][1][6], depth)
+            self.gcode += temp
+            # go through the profile
+            for index in range(0, len(profile)):
+                # true index to work with : we are going to
+                i = (index + closest_index + 1)%len(profile)
+                if profile[i][0] in ['M', 'L']:
+                    temp = """G1 X{} Y{} Z{}\n""".format(profile[i][1][0], profile[i][1][1], depth)
+                if profile[i][0] == 'A':
+                    temp = """G1 X{} Y{} Z{}\n""".format(profile[i][1][5], profile[i][1][6], depth)
+                self.gcode += temp
+        if profile[closest_index][0] in ['M', 'L']:
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][0], profile[closest_index][1][1], properties['clearance_pane'])
+            self.current_position = [profile[closest_index][1][0], profile[closest_index][1][1]]
+        elif profile[closest_index][0] in ['A']:
+            temp += """G0 X{} Y{} Z{}\n""".format(profile[closest_index][1][5], profile[closest_index][1][6], properties['clearance_pane'])
+            self.current_position = [profile[closest_index][1][5], profile[closest_index][1][6]]
+        self.gcode += temp
 
     def engraving(self, svg_path, type, properties):
         '''
@@ -80,9 +163,8 @@ class Machining:
             arguments:
                 - svg_path:str 'd' attribute of your path component
                 - operation_type:str description of the operation : 'profile_inside', 'profile_outside', 'pocket_inside', 'pocket_outside', 'engraving'
-                - properties:dict contains the machining characteristics : target_depth, cut_feedrate, plunge_feedrate, drill_type, drill_radius, depth_increment, stock_surface, holding_tabs_height, holding_tabs_number, holding_tabs_width
+                - properties:dict contains the machining characteristics : target_depth, cut_feedrate, plunge_feedrate, drill_type, drill_radius, depth_increment, stock_surface, clearance_pane, holding_tabs_height, holding_tabs_number, holding_tabs_width
         '''
-        self.gcode += ("ajout d'une gravure de type :\n" + type + "\n")
         self.current_position = [1, 1]
 
     def parse_path(self, svg_path):
@@ -91,5 +173,73 @@ class Machining:
             arguments:
                 - svg_path:str 'd' attribute of your path component
         '''
-        result = []
+        data = []
+        temp1 = ""
+        temp2 = ""
+        for char in svg_path:
+            if char in ['M', 'L', 'A']:
+                if temp1 is not '':
+                    data.append([temp1, temp2])
+                temp1 = char
+                temp2 = ""
+            else:
+                temp2 += char
+        for el in data:
+            el[1] = el[1][1:-2].split(' ')
+            for e in el[1]:
+                e = float(e)
+        result = data
+        return result
+
+    def closest_index(self, profile, position):
+        '''
+        Returns the index of the curve finishing to the closest point to the indicated position.
+            arguments:
+                - profile:list parsed svg path (cf self.parse_path)
+                - position:[float, float] coordinates in 2D or a point
+        '''
+        closest_index = 0
+        min_d = -1
+        for i in range(0, len(profile)):
+            if profile[i][0] in ['M', 'L']:
+                d = math.sqrt((float(position[0]) - float(profile[i][1][0]))**2 + (float(position[1]) - float(profile[i][1][1]))**2)
+            elif profile[i][0] == 'A':
+                d = math.sqrt((float(position[0]) - float(profile[i][1][5]))**2 + (float(position[1]) - float(profile[i][1][6]))**2)
+            else:
+                print(profile[i][0])
+            if min_d == -1:
+                min_d = d
+                closest_index = i
+            elif d < min_d:
+                min_d = d
+                closest_index = i
+        return closest_index
+
+    def define_properties(self, properties):
+        '''
+        Defines the set of properties for a machining operation, to avoid any empty information during calculation.
+            arguments:
+                - properties:dict properties for a machining operation.
+        '''
+        result = {
+        'target_depth' : 0 if 'target_depth' not in properties.keys() else properties['target_depth'],
+        'cut_feedrate' : 0 if 'cut_feedrate' not in properties.keys() else properties['cut_feedrate'],
+        'plunge_feedrate' : 0 if 'plunge_feedrate' not in properties.keys() else properties['plunge_feedrate'],
+        'drill_type' : 'straight' if 'drill_type' not in properties.keys() else properties['drill_type'],
+        'drill_radius' : 8 if 'drill_radius' not in properties.keys() else properties['drill_radius'],
+        'depth_increment' : 0.1 if 'depth_increment' not in properties.keys() else properties['depth_increment'],
+        'stock_surface' : 0 if 'stock_surface' not in properties.keys() else properties['stock_surface'],
+        'clearance_pane' : 20 if 'clearance_pane' not in properties.keys() else properties['clearance_pane'],
+        'holding_tabs_width' : 10 if 'holding_tabs_width' not in properties.keys() else properties['holding_tabs_width'],
+        'holding_tabs_height' : 10 if 'holding_tabs_height' not in properties.keys() else properties['holding_tabs_height'],
+        'holding_tabs_number' : 3 if 'holding_tabs_number' not in properties.keys() else properties['holding_tabs_number']
+        }
+
+        # target depth should always be negative
+        if result['target_depth'] > 0:
+            result['target_depth'] = -1 * result['target_depth']
+        # depth increment should always be negative
+        if result['depth_increment'] > 0:
+            result['depth_increment'] = -1 * result['depth_increment']
+
         return result
--- svgpygcode/temp.py
+++ svgpygcode/temp.py
@@ -2,9 +2,10 @@
 
 import svgpygcode as spg
 from xml.dom import minidom
+import os
 
 # opening svg file
-doc = minidom.parse('/Users/baulieu/scripts/libraries/svgpygcode/svgpygcode/TeamDesk_1.6_Pied35.svg')  # parseString also exists
+doc = minidom.parse('/Users/baulieu/scripts/libraries/svgpygcode/svgpygcode/TeamDesk_pied_35.svg')  # parseString also exists
 path_strings = [[path.getAttribute('d'), path.getAttribute('stroke') ] for path in doc.getElementsByTagName('path')]
 doc.unlink()
 
@@ -12,16 +13,20 @@ machining = spg.Machining()
 
 for path in path_strings:
     if path[1][7:10] == '200':
-        machining.add_operation(path[0], 'pocket_outside', dict())
+        machining.add_operation(path[0], 'pocket_outside', {'clearance_pane':30, 'target_depth':-25, 'depth_increment':-3.1})
     elif path[1][7:10] == '100':
-        machining.add_operation(path[0], 'pocket_inside', dict())
+        machining.add_operation(path[0], 'pocket_inside', {'clearance_pane':30, 'target_depth':-25, 'depth_increment':-3.1})
     if path[1][4:7] == '200':
-        machining.add_operation(path[0], 'profile_inside', dict())
+        machining.add_operation(path[0], 'profile_inside', {'clearance_pane':30, 'target_depth':-35.3, 'depth_increment':-3.1})
     elif path[1][4:7] == '100':
-        machining.add_operation(path[0], 'profile_outside', dict())
+        machining.add_operation(path[0], 'profile_outside', {'clearance_pane':30, 'target_depth':-35.3, 'depth_increment':-3.1})
     elif path[1][4:7] == '255':
-        machining.add_operation(path[0], 'engraving')
+        machining.add_operation(path[0], 'engraving', {'clearance_pane':30, 'target_depth':-25, 'depth_increment':-3.1})
 
 machining.calculate()
 
 print(machining.gcode)
+
+file = open(os.path.join('/Users/baulieu/scripts/libraries/svgpygcode/svgpygcode/', 'result.nc'), 'w+')
+file.write(machining.gcode)
+file.close()
